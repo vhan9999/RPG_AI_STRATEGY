11a02aeac73047482a8dca87c2b8acb60cd59ed5
thaksin5 <128176822+GaoWeiChang@users.noreply.github.com>
2024-04-02 01:58:06 +0800

    create heuristic

--- Assets/Script/WarriorAgent.cs
+++ Assets/Script/WarriorAgent.cs
@@ -18,8 +18,18 @@ public class WarriorAgent : Agent
     public float rotateSpeed = 150f;
     private int rotateDir = 0;
     public Sword sword;
+    [SerializeField] private Transform target;
+
+
+    public override void OnEpisodeBegin()
+    {
+        transform.localPosition = new Vector3(Random.Range(-6f, 7f), 1f, Random.Range(-7f, -4f));
+        target.localPosition = new Vector3(Random.Range(-6f, 7f), 0.5f, Random.Range(2f, 6f));
+    }
+    
     private void Update()
     {
+    
         if (Input.GetMouseButtonDown(0))
         {
             sword.Slash();
@@ -37,11 +47,6 @@ public class WarriorAgent : Agent
         
     }
     public override void Initialize()
-    {
-        
-    }
-
-    public override void OnEpisodeBegin()
     {
         
     }
@@ -79,22 +84,56 @@ public class WarriorAgent : Agent
         {
             rotateDir = 0;
         }
+
     }
 
     public override void OnActionReceived(ActionBuffers actions)
     {
-        
+        int moveAction = actions.DiscreteActions[0];
+        int rotateAction = actions.DiscreteActions[1];
+        //int attackAction = actions.DiscreteActions[2];
+
+        // move forward and backward
+        if (moveAction == 1) ctrlDir += transform.forward;
+        else if (moveAction == 2) ctrlDir -= transform.forward;
+
+        // rotate
+        if (rotateAction == 1) rotateDir = -1;
+        else if (rotateAction == 2) rotateDir = 1;
+
+
+        // attack
+        if (actions.DiscreteActions[3] == 1) sword.Slash();
     }
 
     public override void CollectObservations(VectorSensor sensor)
     {
-        
+        // Agent's position
+        sensor.AddObservation(transform.localPosition);
+
+        // Target's position
+        sensor.AddObservation(target.localPosition);
+
+        // Agent's velocity
+        sensor.AddObservation(GetComponent<Rigidbody>().velocity);
+
+        // Agent's forward direction (assuming a flat environment, using only x and z)
+        sensor.AddObservation(transform.forward.x);
+        sensor.AddObservation(transform.forward.z);
     }
 
-    // compare tag, whether hit ot not (reward)
+
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Character");
-    }
+        if ((other.gameObject.CompareTag("Opponent")) && (sword.anim.GetBool("isAttack"))) {
+            AddReward(10f);
+            EndEpisode();
+        }
+        else if (other.gameObject.CompareTag("Wall"))
+        {
+            AddReward(-2f);
+            EndEpisode();
+        }
 
+    }
 }
