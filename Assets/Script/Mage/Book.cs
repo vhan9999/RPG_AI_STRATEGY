71d3b64e5d89def0b445fd46e802fe19c38d4a67
vhan9999 <vhan9999@gmail.com>
2024-05-03 19:23:09 +0800

    mage done

--- Assets/Script/Mage/Book.cs
+++ Assets/Script/Mage/Book.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Book : MonoBehaviour
 {
     [HideInInspector]
     public bool IsAttack = false;
+    public bool IsSkill = false;
+    public bool IsCoolDown = false;
+    private FireBallCast fireBallCast;
+    [SerializeField] private GameObject fireBall;
     [SerializeField] private GameObject magicMissile;
 
     private ObjectPool<MagicMissile> magicMissilePool;
@@ -13,6 +19,7 @@ public class Book : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        fireBallCast = transform.GetChild(0).GetChild(0).GetComponent<FireBallCast>();
         magicMissilePool = ObjectPool<MagicMissile>.instance;
         magicMissilePool.InitPool(magicMissile, 30);
         agent = GetComponentInParent<ClassAgent>();
@@ -26,7 +33,7 @@ public class Book : MonoBehaviour
 
     public void NormalAttack()
     {
-        if (!IsAttack)
+        if (!IsAttack && !IsSkill)
         {
             IsAttack = true;
             Invoke("ResetAttack", 0.5f);
@@ -41,5 +48,28 @@ public class Book : MonoBehaviour
         m.agent = agent;
         m.Reset();
         IsAttack = false;
+    }
+
+    public void Skill()
+    {
+        if (!IsAttack && !IsCoolDown)
+        {
+            fireBallCast.CastStart();
+            IsCoolDown = true;
+            IsSkill = true;
+            Invoke("CoolDown", 1f);
+        }
+    }
+    private void CoolDown()
+    {
+        IsCoolDown = false;
+    }
+
+    public void Shoot()
+    {
+        IsSkill = false;
+        GameObject f = Instantiate(fireBall, transform.position + transform.up, transform.rotation);
+        f.GetComponent<FireBall>().moveDir = transform.forward;
+        f.GetComponent<FireBall>().agent = agent;
     }
 }
