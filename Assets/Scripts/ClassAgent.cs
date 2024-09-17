dd1eb6d9496e8e87613be66ef09b9ffa151e7cc9
vhan9999 <vhan9999@gmail.com>
2024-09-17 22:07:18 +0800

    adjust archer draw speed,add fire power input, add is dizzy input

--- Assets/Scripts/ClassAgent.cs
+++ Assets/Scripts/ClassAgent.cs
@@ -223,6 +223,7 @@ public class ClassAgent : Agent
         sensor.AddObservation(currentHealth);
         sensor.AddObservation(isDead);
         sensor.AddObservation(inputSpeed);
+        sensor.AddObservation(isDizzy);
     }
 
     private void OnTriggerEnter(Collider other)
